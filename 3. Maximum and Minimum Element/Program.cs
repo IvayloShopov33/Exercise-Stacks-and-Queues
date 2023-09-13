5d37180ab55fe1a1727b2001b904cac47d6000a0
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-13 17:15:40 +0300

    Update Program.cs

--- 3. Maximum and Minimum Element/Program.cs
+++ 3. Maximum and Minimum Element/Program.cs
@@ -13,13 +13,14 @@
                 if (numbers[0] == 1)
                     stackOfNumbers.Push(numbers[1]); //push number to the stack
                 else if (numbers[0] == 2)
-                    stackOfNumbers.Pop(); //delete the element at the top of the stack
+                    stackOfNumbers.Pop(); //remove the last entered element from the stack
                 else if (numbers[0] == 3 && stackOfNumbers.Count>0)
                     Console.WriteLine(stackOfNumbers.Max()); //print the biggest number in the stack
                 else if (numbers[0] == 4 && stackOfNumbers.Count>0)
                     Console.WriteLine(stackOfNumbers.Min()); //print the smallest number in the stack
             }
-            Console.WriteLine(string.Join(", ", stackOfNumbers)); //print the stack
+            
+            Console.WriteLine(string.Join(", ", stackOfNumbers));
         }
     }
 }

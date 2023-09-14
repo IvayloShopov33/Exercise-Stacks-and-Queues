464013fd22279f68c5417508fe6f5193ac57f23f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-14 19:35:24 +0300

    Update Program.cs

--- 12. Cups and Bottles/Program.cs
+++ 12. Cups and Bottles/Program.cs
@@ -9,32 +9,40 @@
             int[] bottlesValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
             foreach (int bottle in bottlesValues)
             {
-                bottles.Push(bottle); //push the bottle to the stack of bottles
+                bottles.Push(bottle);
             }
+            
             int wastedLittersOfWater = 0;
             while (cupsValues.Count > 0 && bottles.Count > 0)
             {
-                int bottle = bottles.Pop(); //remove the bottle from the stack
-                int cup = cupsValues.First(); //takes the first cup
+                int bottle = bottles.Pop();
+                int cup = cupsValues.First(); 
                 cupsValues.RemoveAt(0);
-                if (bottle >= cup) //check if the bottle has more litters of water than the cup
+
+                 //check if the bottle has more litters of water than the cup
+                if (bottle >= cup)
                 {
                     wastedLittersOfWater += bottle - cup;
                 }
                 else
                 {
+                    //return the cup to the list of cups with the new value
                     cup -= bottle;
-                    cupsValues.Insert(0, cup); //return the cup to the list of cups with the new value
+                    cupsValues.Insert(0, cup); 
                 }
             }
 
-            if (cupsValues.Count == 0) // check if there are remained cups 
+            // check if there are remained cups 
+            if (cupsValues.Count == 0) 
+            {
                 Console.WriteLine($"Bottles: {string.Join(' ', bottles)}");
-            else
+            }               
+            else 
+            {
                 Console.WriteLine($"Cups: {string.Join(' ', cupsValues)}");
-
-
-            Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}"); //print the wasted litters of water
+            }
+                
+            Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}"); 
         }
     }
 }

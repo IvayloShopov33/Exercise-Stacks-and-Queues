639660991a5ab8e68734900143143e96b69186e8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-14 19:21:11 +0300

    Update Program.cs

--- 9. Simple Text Editor/Program.cs
+++ 9. Simple Text Editor/Program.cs
@@ -6,38 +6,45 @@ namespace _9._Simple_Text_Editor
     {
         static void Main(string[] args)
         {
-            int commandsCount = int.Parse(Console.ReadLine());
-            StringBuilder text = new StringBuilder();
             Stack<string> previousStatesOfTheText = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+            int commandsCount = int.Parse(Console.ReadLine());
             for (int i = 1; i <= commandsCount; i++)
             {
                 string[] commands = Console.ReadLine().Split();
                 int commandNumber = int.Parse(commands[0]);
+                
                 if (commandNumber == 1)
                 {
+                    //add a string to the end of the stringBuilder
                     string textToAppend = commands[1];
-                    text.Append(textToAppend); //appends someString to the end of the text
+                    text.Append(textToAppend);
                     previousStatesOfTheText.Push(text.ToString());
                 }
                 else if (commandNumber == 2)
                 {
+                    //remove last N characters from the stringBuilder
                     int countOfCharsToDelete = int.Parse(commands[1]);
-                    text = text.Remove(text.Length - countOfCharsToDelete, countOfCharsToDelete); //erases the last count elements from the text
+                    text = text.Remove(text.Length - countOfCharsToDelete, countOfCharsToDelete);
                     previousStatesOfTheText.Push(text.ToString());
                 }
                 else if (commandNumber == 3)
                 {
+                    //search and print the character at that index
                     int index = int.Parse(commands[1]);
-                    Console.WriteLine(text[index - 1]); //returns the element at position index from the text
+                    Console.WriteLine(text[index - 1]);
                 }
                 else if (commandNumber == 4)
                 {
                     //undoes the last not undone command of type 1 / 2 and returns the text to the state before that operation
                     previousStatesOfTheText.Pop();
-                    text = text.Clear(); //removes all characters from the string
-                    if (previousStatesOfTheText.Count > 0) //check if stack is (not) empty
+                    text = text.Clear();
+                    
+                    //check if stack is (not) empty
+                    if (previousStatesOfTheText.Count > 0) 
                     {
-                        text.Append(previousStatesOfTheText.Peek()); //appends text from previous command
+                        //appends text from previous command
+                        text.Append(previousStatesOfTheText.Peek()); 
                     }
                 }
             }

a8856b254daf1a772c49e6b9c8b734e803b03ef7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-14 19:30:26 +0300

    Update Program.cs

--- 11. Key Revolver/Program.cs
+++ 11. Key Revolver/Program.cs
@@ -4,26 +4,28 @@
     {
         static void Main(string[] args)
         {
+            Stack<int> bullets = new Stack<int>();
+            Queue<int> locks = new Queue<int>();
             int pricePerBullet=int.Parse(Console.ReadLine());
             int sizeOfGunBarrel=int.Parse(Console.ReadLine());
             int[] bulletsSize=Console.ReadLine().Split().Select(int.Parse).ToArray();
-            Stack<int> bullets = new Stack<int>();
             int[] locksSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            Queue<int> locks = new Queue<int>();
             int valueOfIntelligence = int.Parse(Console.ReadLine());
             foreach (int bullet in bulletsSize)
             {
                 bullets.Push(bullet); //push the bullets to the stack
             }
+            
             foreach (int @lock in locksSize)
             {
                 locks.Enqueue(@lock); // put the lock in the queue
             }
+            
             int usedBulletsCount = 0;
             while (bullets.Count>0 && locks.Count>0)
             {
-                int bullet=bullets.Pop(); // remove the bullets from the stack
-                int @lock=locks.Peek(); // takes the first lock
+                int bullet=bullets.Pop();
+                int @lock=locks.Peek();
                 if (bullet <= @lock)
                 {
                     locks.Dequeue(); //remove the lock from the queue
@@ -35,11 +37,14 @@
                 }
 
                 usedBulletsCount++;
-                if (usedBulletsCount%sizeOfGunBarrel==0 && bullets.Count>0)
+                if (usedBulletsCount%sizeOfGunBarrel==0 && bullets.Count>0) 
+                {
                     Console.WriteLine("Reloading!");
+                }
             }
 
-            if (locks.Count>0) //check if all the locks are removed from the queue
+            //check if all locks are removed from the queue
+            if (locks.Count>0) 
             {
                 Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
             }
